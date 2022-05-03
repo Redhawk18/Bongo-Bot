1719bdb4163d6b17484378fda6985d28dd7ec0a0
Redhawk18 <Redhawk76767676@gmail.com>
2022-05-03 16:37:01 -0400

    async problem :(

--- commands/music_commands.py
+++ commands/music_commands.py
@@ -1,4 +1,5 @@
 import os
+from queue import Queue
 
 import discord
 from discord.ext import commands
@@ -6,9 +7,11 @@ from discord.ext.commands.errors import ClientException
 from yt_dlp import YoutubeDL
 
 class Music_Commands(commands.Cog):
+    q = Queue(0)
 
     def __init__(self, client):
         self.client = client
+        
 
     #events
     @commands.Cog.listener()
@@ -22,7 +25,7 @@ class Music_Commands(commands.Cog):
         if voice.is_connected():
             await voice.disconnect()
             await ctx.send("**Disconnected** :guitar:")
-            
+
         else:
             await ctx.send("Already disconnected")
 
@@ -33,46 +36,55 @@ class Music_Commands(commands.Cog):
         #TODO handle error when user isnt in vc
         #TODO look into why the video some times has noise
         #TODO add queue
+        print(Music_Commands.q.qsize())
         song_valid = os.path.isfile("song.opus")
         try:
             if song_valid: #no song is playing, removing old song file
                 os.remove("song.opus")
 
+            Music_Commands.q.put(url)
+
         except PermissionError: #if a current song is playing
             await ctx.send("song is playing, i havent put a queue system in yet") 
+            Music_Commands.q.put(url)
+            print(Music_Commands.q.qsize())
             return
 
-        #find current vc
-        voiceChannel = ctx.author.voice.channel
-        try: #connect to channel
-            await voiceChannel.connect()
-            voice = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
-            await ctx.send("**Connected** :drum: to `" + str(voiceChannel) +"`")
 
-        except ClientException: #already connected
-            voice = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
         
+        while Music_Commands.q is not Music_Commands.q.empty():
+            #find current vc
+            voiceChannel = ctx.author.voice.channel
+            try: #connect to channel
+                await voiceChannel.connect()
+                voice = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
+                await ctx.send("**Connected** :drum: to `" + str(voiceChannel) +"`")
 
-        ydl_opts = { 
-            'format': 'bestaudio',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'opus',
-                'preferredquality': '256',
-            }],
-        }
+            except ClientException: #already connected
+                voice = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
+            
 
-        with YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-            info_dict = ydl.extract_info(url, False)
+            ydl_opts = { 
+                'format': 'bestaudio',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'opus',
+                    'preferredquality': '256',
+                }],
+            }
 
-        for file in os.listdir("./"):
-            if file.endswith(".opus"):
-                os.rename(file, "song.opus")
+            current_url = Music_Commands.q.get()
+            with YoutubeDL(ydl_opts) as ydl:
+                ydl.download([current_url])
+                info_dict = ydl.extract_info(current_url, False)
 
-        voice.play(discord.FFmpegPCMAudio("song.opus"))
-        output = "**Playing** :notes: `" + info_dict.get('title', None) + "` - Now!"
-        await ctx.send(output)
+            for file in os.listdir("./"):
+                if file.endswith(".opus"):
+                    os.rename(file, "song.opus")
+
+            voice.play(discord.FFmpegPCMAudio("song.opus"))
+            output = "**Playing** :notes: `" + info_dict.get('title', None) + "` - Now!"
+            await ctx.send(output)
 
     @commands.command()
     async def pause(self, ctx):
