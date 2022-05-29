2312253378f6cd20e36ab0a3f42629bb094b3675
Redhawk18 <Redhawk76767676@gmail.com>
2022-05-29 18:28:23 -0400

    react to a user in .env file

--- commands/useless_commands.py
+++ commands/useless_commands.py
@@ -1,7 +1,9 @@
+import os
 import random
 
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
 
 class Useless_Commands(commands.Cog):
 
@@ -30,6 +32,26 @@ class Useless_Commands(commands.Cog):
     @commands.command()
     async def frank(self, ctx):
         await ctx.send('https://tenor.com/view/gezer123123123-gif-18858197')
+
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        #get trolled user from env
+        load_dotenv()
+        TROLLED_USER = os.getenv('TROLLED_USER')
+
+        if message.author.name + "#" + message.author.discriminator != TROLLED_USER:
+            return
+
+        if message.content.startswith('!'):
+            return
+
+        #1 in 20 messages
+        if random.randint(1, 20) == 20:
+            await message.add_reaction("🐧")
+
+
+
 
 
 
