d45836c25facd7b64755157f30e284f6452a596a
Redhawk18 <Redhawk76767676@gmail.com>
2022-07-07 20:40:18 -0400

    updated to work with 2.0 slash commands

--- commands/useless_commands.py
+++ commands/useless_commands.py
@@ -2,6 +2,7 @@ import os
 import random
 
 import discord
+from discord import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -16,22 +17,9 @@ class Useless_Commands(commands.Cog):
         print("useless commands lister online")
 
 
-    @commands.command()
-    async def quote(self, ctx):
-        epic_quotes = [
-        'Mimosa is literally orihime',
-        'Hi Ben jojolion isnt coming out till the 19th, feelsbadman\n Wait part 6 ended this month?\n part 6 ended in 2005',
-        'hi guys\n Why\n https://tenor.com/view/xqc-arabfunny-arabic-saudi-arabia-projared-gif-18306091',
-        'https://tenor.com/view/gezer123123123-gif-18858197',
-        'https://tenor.com/view/stop-hitting-my-child-in-gif-25029977'
-        ]    
-        response = random.choice(epic_quotes)
-        await ctx.send(response)
-
-
-    @commands.command()
-    async def frank(self, ctx):
-        await ctx.send('https://tenor.com/view/gezer123123123-gif-18858197')
+    @app_commands.command()
+    async def frank(self, interaction: discord.Interaction):
+        await interaction.response.send_message('https://tenor.com/view/gezer123123123-gif-18858197')
 
 
     @commands.Cog.listener()
@@ -49,9 +37,6 @@ class Useless_Commands(commands.Cog):
         #1 in 20 messages
         if random.randint(1, 20) == 20:
             await message.add_reaction("🐧")
-
-
-
 
 
 
