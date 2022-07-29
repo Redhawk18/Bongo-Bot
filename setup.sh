60703ee8e1190f2526f1908926c89efa97b96a06
Redhawk18 <Redhawk76767676@gmail.com>
2022-07-29 18:15:33 -0400

    updated lavalink and depences

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ mkdir lavalink
 cd lavalink
 
 #download lavalink
-curl -LJO https://github.com///freyacodes/Lavalink/releases/download/3.4/Lavalink.jar
+#curl -LJO https://github.com///freyacodes/Lavalink/releases/download/3.4/Lavalink.jar
+echo "get the dev branch of lavalink on your own"
 #create BLANK config file
 touch application.yml
