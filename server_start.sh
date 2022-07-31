a42d5fd924493b02bdc097c5b987944590d6e155
Redhawk18 <Redhawk76767676@gmail.com>
2022-07-30 20:04:01 -0400

    updated and created more scripts

---
+++ server_start.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+#Lavalink WILL NOT WORK IF NOT RUN IN THE LAVALINK DIR
+cd lavalink
+java -jar lavalink.jar
--- setup.sh
+++
@@ -1,9 +0,0 @@
-#make lavalink dir
-mkdir lavalink
-cd lavalink
-
-#download lavalink
-#curl -LJO https://github.com///freyacodes/Lavalink/releases/download/3.4/Lavalink.jar
-echo "get the dev branch of lavalink on your own"
-#create BLANK config file
-touch application.yml
