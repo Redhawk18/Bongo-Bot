84409ec8c812e053f4dfaa55a532128b824ed8e2
Redhawk18 <Redhawk76767676@gmail.com>
2022-08-15 19:38:39 -0400

    updated lavalink version

--- server_setup.bat
+++ server_setup.bat
@@ -3,6 +3,6 @@ mkdir lavalink
 cd lavalink
 
 :: download lavalink
-PowerShell wget "https://github.com/freyacodes/Lavalink/releases/download/3.5-rc2/Lavalink.jar -O Lavalink.jar"
+PowerShell wget "https://github.com/freyacodes/Lavalink/releases/download/3.5-rc3/Lavalink.jar -O Lavalink.jar"
 
 echo Lavalink setup complete
