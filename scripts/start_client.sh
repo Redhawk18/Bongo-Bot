a49ec2300a2d7cb76edb53b0f85bce49fc9d339d
Carson <redhawk@Paisley-Park.Paisley-Park>
2023-03-01 00:45:47 -0500

    fixed client script

--- scripts/start_client.sh
+++ scripts/start_client.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
+#go to script path
+cd $( cd -- "$(dirname "$0")" > /dev/null 2>&1 ; pwd -P )
+cd ..
+
 git pull
 pip3 install -U -r requirements.txt
 
-python3 ../src/main.py
+python3 src/main.py
