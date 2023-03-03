dad9f32eb9ec1e8362812408a687911c0c4b93be
redhawk18 <redhawk76767676@gmail.com>
2023-03-03 01:53:07 -0500

    smart switch between local and docker

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import asyncio
+import os
 from os import getenv
 import logging
 from pathlib import Path
+import sys
 
 import discord
 from dotenv import load_dotenv
@@ -13,11 +15,13 @@ discord.utils.setup_logging(level=logging.INFO)
 log = logging.getLogger(__name__)
 
 root_path = Path(__file__).parent.resolve().parent.resolve()
-if not Path.is_file(root_path.joinpath(".env")):
-    log.critical("you forgot the .env file")
-    exit()
+if len(sys.argv) < 2: #add everything to environ
+    if not Path.is_file(root_path.joinpath(".env")):
+        log.critical("you forgot the .env file")
+        exit()
 
-load_dotenv(root_path.joinpath(".env"))
+    load_dotenv(root_path.joinpath(".env"))
+
 TOKEN = getenv('DISCORD_TOKEN')
 
 bot = Bongo_Bot()
