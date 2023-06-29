eeeefb035ad06fd1f4cb3e616fbdcee1665251fa
Redhawk18 <redhawk76767676@gmail.com>
2023-06-29 19:10:35 -0400

    wavelink handles the current track

--- src/cache.py
+++ src/cache.py
@@ -7,7 +7,6 @@ class Cache:
 
     def __init__(self):
         # internal variables
-        self.now_playing_track = None
         self.playing_view = None
         self.playing_view_channel = None
         self.playing_view_message = None
@@ -17,17 +16,3 @@ class Cache:
         self.music_channel_id = None
         self.music_role_id = None
         self.volume = 25
-
-    def __str__(self):
-        return f"""
-        now playing, {self.now_playing_track}
-        playing view channel id, {self.playing_view_channel_id}
-        playing view message id, {self.playing_view_message_id}
-        playing_view, {self.playing_view}
-        song queue, {self.queue}
-        user who want to skip, {self.user_who_want_to_skip}
-
-        music_channel_id, {self.music_channel_id}
-        music_role_id, {self.music_role_id}
-        volume, {self.volume}
-        """
